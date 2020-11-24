0a818100e7599958047ebc85fcde34d863b684b1
tonyalcorn <xmastony@gmail.com>
2020-11-23 16:08:05 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'YOUR-NAME is the champion'
+
+const main = document.querySelector('main')
+
+main.remove()
